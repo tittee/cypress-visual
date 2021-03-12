fd87e8c799b97adde87239c27b86d51dff90a64a
Bluenarysun <tee.emilond@gmail.com>
2021-03-12 17:40:38 +0700

    Visaual snapshot image

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot');
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
 
 module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config);
