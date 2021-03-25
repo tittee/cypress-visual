86730e3541db85fccdaa69cb14d9b5f1e9b476a8
Bluenarysun <tee.emilond@gmail.com>
2021-03-25 09:18:46 +0700

    Visual image snapshot and match Element

--- cypress/integration/image.spec.js
+++ cypress/integration/image.spec.js
@@ -1,6 +1,22 @@
+const pages = ['https://example.com'];
+const sizes = ['iphone-6', 'ipad-2', [768, 1200]];
 describe('Visual Regression', () => {
-  it('My first visual regression test', () => {
-    cy.visit('https://example.com');
-    cy.matchImageSnapshot();
+  sizes.forEach(size => {
+    pages.forEach(page => {
+      it(`Shoould match ${page} in resolution ${size}`, () => {
+        let currentTime = new Date(Date.UTC(2021, 3, 25)).getDate();
+        cy.clock(currentTime);
+        cy.setResolution(size);
+        cy.visit(page);
+        cy.matchImageSnapshot();
+      });
+    });
   });
 });
+
+describe('Single Element Snapshot', () => {
+  it("Should  match a single element on the page", () => {
+    cy.visit(pages[0]);
+    cy.get("h1").matchImageSnapshot();
+  })
+})
